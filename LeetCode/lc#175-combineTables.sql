8f900b0e5a98727ad7ac8da3548eb400446fc353
cam673 <36182201+cam673@users.noreply.github.com>
2021-09-18 18:32:18 -0700

    Create lc#175-combineTables.sql

---
+++ LeetCode/lc#175-combineTables.sql
@@ -0,0 +1,5 @@
+# Successful Submission
+# Write your MySQL query statement below
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person
+LEFT JOIN Address ON Person.PersonId=Address.PersonId;
